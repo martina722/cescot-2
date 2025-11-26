621e84c9ba5b45f6795b3f79ba07f8cbc44abbea
martina722 <marticm26@gmail.com>
2025-11-26 11:04:40 +0100

    agiunte modifiche

--- Linguaggio JS/script.js
+++ Linguaggio JS/script.js
@@ -19,3 +19,91 @@ const costante = "ciao Mondo!!" ; /* costante, alla base del framework di JS c'�
 variabile, ma l'oggetto e il suo stato (es. aperto, chiuso), queste informazioni sono all'interno di
 una costante */
 
+// stringhe
+var stringa = "insieme di caratteri!" ;
+console.log(stringa[2]) ;
+console.log(typeof stringa);
+
+// numeri interi
+var intero = 1;
+console.log(typeof intero);
+
+// decimali
+var decimale = 1.2;
+console.log(typeof decimale);
+
+// boolean
+var bool = true;
+console.log(typeof bool);
+
+// array
+var array = [1, 2, 3, 4, 5, 6];
+console.log(typeof array);
+
+// oggetto
+var object = {
+    "nome": "Mario",
+    "eta": "30"
+};
+console.log(typeof object);
+
+// operatori aritmetici
+var a = 1;
+var b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b); // resto della divisione
+
+a = 1;
+console.log(a++); // leggo poi incremento
+console.log(++a); // incremento poi leggo
+
+// operatori logici
+console.log(a < b);
+console.log(a <= c);
+console.log(a == 3 && b == 1); // and
+console.log(a == 3 || b == 1); // or
+console.log(!(a < b)); // not
+console.log(a == 3); //uguaglianza
+console.log(a == "3"); // uguaglianza per valore
+console.log(a === 3); // uguaglianza per tipo e per valore
+
+// concatenazione
+console.log(stringa1 + stringa2);
+
+a = 2;
+b = 3;
+c = "1";
+var somma = a + c;
+
+console.log(a + b); // somma
+console.log(a + c); // concatenazione -> "2 attaccato a 1" = 21
+console.log(somma + b); // a + c = 2 e 1 poi aggiungo il 3, quindi 213
+console.log(somma > 20); // somma viene convertito in numero -> quindi è vero
+console.log(somma.lenght); // lunghezza della stringa
+console.log(a + b + c); // 51, perchè 2+3=5 poi aggiungo "1", quindi 51
+
+var d;
+console.log(d); // tipo di dato indefinito
+d = undefined;
+
+var e = null
+console.log(e); // tipo di dato nullo, prima o poi avrà un valore
+
+var oggetto = {
+    "nullo": null,
+    "undefined": undefined
+};
+console.log(oggetto);
+
+console.log(d == true); // falso
+console.log(e == true); // falso
+console.log(d == e); // entrambi falsi -> true
+console.log(d === e); //null è diverso da undefined
+
+
+
+
