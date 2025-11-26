7e2422d0b51834540c266b274267c7ccd5badaf0
martina722 <marticm26@gmail.com>
2025-11-26 10:23:36 +0100

    .

--- Linguaggio JS/script.js
+++ Linguaggio JS/script.js
@@ -1,3 +1,21 @@
+// stampa in console
 console.log("Ciao mondo") ;
+
+// mostra un pop up con messaggio
 window.alert("Benvenuti") ;
-window.confirm("sei sicuro?")
+
+// pop up con richiesta
+window.confirm("sei sicuro?") ;
+
+// mostra pop up che chiede un dato
+window.prompt("inserisci un numero") ;
+
+// dichiarazione variabile
+var stringa = "Ciao mondo" ; /* variabile globale, quando l'ho dichiarata la vedono 
+tutti all'inerno del file */
+let stringa2 = "ciao mondo!" ; /* variabile locale, se la dichiaro dentro la funzione la vedo solo 
+dentro quella funzione */
+const costante = "ciao Mondo!!" ; /* costante, alla base del framework di JS c'è lo stato non la
+variabile, ma l'oggetto e il suo stato (es. aperto, chiuso), queste informazioni sono all'interno di
+una costante */
+
